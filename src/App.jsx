c8358063886a268d2849074e0190fb6efd647382
Jharanatmg <jharanatmg22@gmail.com>
2023-05-22 21:16:52 -0700

    first phase

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Coffee</h1>
+      <header>
+        <ul>
+          <li>Home</li>
+          <li>Gallery</li>
+          <li>History</li>
+          <li>Culture</li>
+        </ul>
+      </header>
+      <div className="content">
+       
+        <p>
+          Seeds of the Coffea plant's fruits are separated to produce unroasted green coffee beans. The beans are roasted and then ground into fine particles that are typically steeped in hot water before being filtered out, producing a cup of coffee. The word coffee entered the English language in 1582 via the Dutch koffie, borrowed from the Ottoman Turkish kahve (قهوه), borrowed in turn from the Arabic qahwah (قَهْوَة).[4] Medieval Arab lexicographers traditionally held that the etymology of qahwah meant 'wine', given its distinctly dark color, and derived from the verb qahiya (قَهِيَ), 'to have no appetite'.The word qahwah most likely meant 'the dark one', referring to the brew or the bean; qahwah is not the name of the bean, which are known in Arabic as bunn and in Cushitic languages as būn. Semitic languages had the root qhh, 'dark color', which became a natural designation for the beverage. Its cognates include the Hebrew qehe(h) ('dulling') and the Aramaic qahey ('give acrid taste to').
+        </p>
+        <p>
+        The word coffee entered the English language in 1582 via the Dutch koffie, borrowed from the Ottoman Turkish kahve (قهوه), borrowed in turn from the Arabic qahwah (قَهْوَة).Medieval Arab lexicographers traditionally held that the etymology of qahwah meant 'wine', given its distinctly dark color, and derived from the verb qahiya (قَهِيَ), 'to have no appetite'. The word qahwah most likely meant 'the dark one', referring to the brew or the bean; qahwah is not the name of the bean, which are known in Arabic as bunn and in Cushitic languages as būn. Semitic languages had the root qhh, 'dark color', which became a natural designation for the beverage. Its cognates include the Hebrew qehe(h) ('dulling') and the Aramaic qahey ('give acrid taste to').
+        </p>
+      </div>
+      <footer className="bottom">
+      The terms coffee pot and coffee break originated in 1705 and 1952 respectively.even though Arab etymologists connected with a word meaning "wine," but it is perhaps rather from the Kaffa region of Ethiopia. 
+      </footer>
+      <button>
+         <i>Click here to learn more</i> 
+        </button>
+    </>
+  )
+}
+
+export default App
